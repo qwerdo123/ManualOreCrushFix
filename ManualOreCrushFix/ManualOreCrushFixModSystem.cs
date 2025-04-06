873bcc63c67233a72dfb3ff6d3fa63d210a3e64b
qwerdo123 <qwerdo123@gmail.com>
2025-04-06 21:36:44 +0200

    Add project files.

---
+++ ManualOreCrushFix/ManualOreCrushFixModSystem.cs
@@ -0,0 +1,17 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace ManualOreCrushFix
+{
+    public class ManualOreCrushFixModSystem : ModSystem
+    {
+
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+        }
+
+    }
+}
